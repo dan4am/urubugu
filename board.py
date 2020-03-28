230ea65e3bd398d8758dcb723615af45bf3d03a9
dan4am <39918471+dan4am@users.noreply.github.com>
2020-03-28 17:05:00 +0100

    1 one bead move 2 one bead capture opponent beads 3 two players

--- board.py
+++ board.py
@@ -2,10 +2,10 @@ import numpy as np
 from numpy import half
 
 # BOARD = np.zeros((4,8), int)
-BOARD = np.array([ [ 0, 0, 0,  0,  0,  0,  0,  0],
+BOARD = np.array([ [ 0,  0,  0,  4,  4,  4,  4,  0],
+                   [ 0,  0,  0,  4,  4,  4,  4,  0],
                    [ 4,  4,  4,  4,  4,  4,  4,  4],
-                   [ 0,  4,  4,  4,  4,  0,  0,  0],
-                   [ 0,  4,  4,  4,  4,  0,  0,  0]])
+                   [ 0,  0,  0,  0,  0,  0,  0,  0]])
 MAX_INIT = 4
 MAX_BEADS = 64
 player_one = True
@@ -68,6 +68,38 @@ def play(hole):
     current_hole = hole
     tmp_beads = beads(current_hole)
     loop = 0
+    if (tmp_beads == 1):
+        take_beads(current_hole,1)
+        current_hole += 1
+        add_bead(current_hole)
+        tmp_beads = beads(current_hole)
+        print(BOARD)
+        if(current_hole > 8):
+            result3 = hole_correspondance(current_hole)
+            row2_crsp = result3[1]
+            row1_crsp = result3[0]
+
+            if (player_one):
+                player(2)
+            else:
+                player(1)
+
+            empty_p2_holes = beads(row1_crsp) == 0 and beads(row2_crsp) == 0
+            if (not empty_p2_holes):
+                beads_row1 = beads(row1_crsp)
+                beads_row2 = beads(row2_crsp)
+                tmp_beads = beads_row1 + beads_row2
+                take_beads(row1_crsp, beads_row1)
+                take_beads(row2_crsp, beads_row2)
+                current_hole -= 1
+                if (not player_one):
+                    player(1)
+                else:
+                    player(2)
+            else:
+                if (not player_one):
+                    player(1)
+
     while(not beads(current_hole)== 1):
         loop += 1
         print (loop)
@@ -82,7 +114,10 @@ def play(hole):
             result2 = hole_correspondance(temp_hole)
             row2_crsp = result2[1]
             row1_crsp = result2[0]
-            player(2)
+
+            if(player_one) :player(2)
+            else: player(1)
+
             empty_p2_holes = beads(row1_crsp) == 0 and beads(row2_crsp) == 0
             if(not empty_p2_holes):
                 beads_row1 = beads(row1_crsp)
@@ -90,31 +125,62 @@ def play(hole):
                 tmp_beads = beads_row1 + beads_row2
                 take_beads(row1_crsp,beads_row1)
                 take_beads(row2_crsp,beads_row2)
-                player(1)
+                if (not player_one):player(1)
+                else:player(2)
             else:
-                player(1)
+                if (not player_one):
+                    player(1)
+                else:
+                    player(2)
                 current_hole = temp_hole
                 tmp_beads = beads(current_hole)
         else:
-            player(1)
+            # if (not player_one):
+            #     player(1)
+            # else:
+            #     player(2)
             current_hole = temp_hole
             tmp_beads = beads(current_hole)
+        print(BOARD)
+
+def game_over():
+    gameover=True
+    for hole in range (1, 16):
+        gameover = gameover and beads(hole) == 0
+    return gameover
+
+def two_players():
+    while (not game_over()):
+        if(player_one):
+            slot =int ( input("player one choose slot between 1 and 16 : "))
+            result = beads(slot)
+            while(result == 0):
+                slot = int(input("player one choose another slot between 1 and 16 that one is empty : "))
+                result = beads(slot)
+            play(slot)
+            player(2)
+        else:
+            slot = int(input("player two choose slot between 1 and 16 : "))
+            result = beads(slot)
+            while (result == 0):
+                slot = int(input("player twoo choose another slot between 1 and 16 that one is empty : "))
+                result = beads(slot)
+            play(slot)
+            player(1)
+
+    if player_one:
+        print("player two wins")
+    else:
+        print("player one wins")
 
 
 
 def main():
 
     player(1)
-    # BOARD[2][3] = 0
-    # BOARD[3][3] = 9
-    # # BOARD[3][7] = 1
-    # BOARD[0][3] = 5
-    # BOARD[0][2] = 17
-    # BOARD[1][3] = 5
-    print(BOARD)
-    # print(beads(13))
-    # print(beads(4))
-    play(5)
+    two_players()
+    # play(1)
+    # play(6)
     print(BOARD)
 if __name__ == '__main__':
     main()
